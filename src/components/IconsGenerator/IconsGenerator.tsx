bfb17cde6a299bdfc6e1ef463e561f6f92659a53
Sergey <serzh.kachalyuk@gmail.com>
2024-04-11 09:04:17 +0300

    Add preloader

--- src/components/IconsGenerator/IconsGenerator.tsx
+++ src/components/IconsGenerator/IconsGenerator.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import classes from './IconsGenerator.module.css'
+import Preloader from '../../assets/Preloader.svg'
 
 // Library imports
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
@@ -9,19 +10,31 @@ import { faRandom } from '@fortawesome/free-solid-svg-icons'
 // Utility to generate icons
 import { FontAwesomeHelper } from "../../utils/FontAwesomeHelper";
 
+
 export const IconsGenerator: React.FC = ({ }) => {
     const [icon, setIcon] = useState<IconDefinition>(faRandom);
+    const [isLoading, setLoading] = useState<boolean>(false);
+
 
     const generateIcon = () => {
+        setLoading(true);
         setTimeout(() => {
             const icon = FontAwesomeHelper.getRandomIcon();
             setIcon(icon);
+            setLoading(false);
         }, 3000);
-    }
+    };
 
     return (
         <div className={classes.parent}>
-            <FontAwesomeIcon icon={icon} className={classes.icon} />
+            <div className={classes.iconWrapper}>
+                {isLoading
+                    ?
+                    <img src={Preloader} alt="Loading..." className={classes.preloader} />
+                    :
+                    <FontAwesomeIcon icon={icon} className={classes.icon} />
+                }
+            </div>
             <button onClick={generateIcon} className={classes.btn}>Generate</button>
         </div>
     );
--- src/utils/FontAwesomeHelper.ts
+++ src/utils/FontAwesomeHelper.ts
@@ -1,10 +1,11 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { MathHelper } from './MathHelper';
 
 // To simplify the task I've selected only solid icons type. We can add any more
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
 export const FontAwesomeHelper = {
-    getRandomIcon() {
+    getRandomIcon(): IconDefinition {
         // Find a random key of the 'fas' collection
         const fasKeys = Object.keys(fas);
         const randomIndex = MathHelper.getRandomNumberInRange(0, fasKeys.length - 1);
